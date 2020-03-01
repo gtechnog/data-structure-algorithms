0ea0ca22a7d075c87e7d928659ff002a26a88a0b
Gaurav Gupta <gaurav.gupta@walmartlabs.com>
2020-03-01 16:17:39 +0530

    updated comments

--- src/leetcode/1_Two_Sum.kt
+++ src/leetcode/1_Two_Sum.kt
@@ -8,13 +8,23 @@ package leetcode
 
 class Solution {
     fun twoSum(numbers: IntArray, target: Int) : IntArray {
+
+        // This map will be used to store the occurrence of elements
         val dataMap = mutableMapOf<Int, Int>()
+
+        // Iterate over the numbers
         for ((index, number) in numbers.withIndex()) {
+
+            // If number is present in map already means we found both the element (current element & element present in map)
             if (dataMap.containsKey(target - number)) {
                 return intArrayOf(index, dataMap.getValue(target-number))
             }
+
+            // If number not present in map, add this element to map, might be useful for other elements
             dataMap[number] = index
         }
+
+        // No combination found in the array, so returning empty array
         return intArrayOf()
     }
 }
